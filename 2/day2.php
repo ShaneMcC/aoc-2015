d47235ecb4dbf863fb7a1b13d25c1ba567d486ea
Shane Mc Cormack <shanemcc@gmail.com>
2015-12-02 10:54:32 +0000

    Day 2.

---
+++ 2/day2.php
@@ -0,0 +1,23 @@
+<?php
+
+	$lines = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+//	$lines = file('test.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	$total = 0;
+	$ribbontotal = 0;
+	foreach ($lines as $line) {
+		list($l, $w, $h) = explode('x', $line);
+		$paper = (2*$l*$w) + (2*$w*$h) + (2*$h*$l);
+		$dim = array($l, $w, $h);
+		sort($dim);
+		$slack = $dim[0] * $dim[1];
+		$total += $paper + $slack;
+
+		$ribbon = ($dim[0]*2) + ($dim[1]*2) + ($l*$w*$h);
+		$ribbontotal += $ribbon;
+
+		echo sprintf('[%dx%dx%d] %d + %d = %d [%d] {%d => %d}', $l, $w, $h, $paper, $slack, ($paper+$slack), $total, $ribbon, $ribbontotal), "\n";
+	}
+
+	echo "\n\n";
+	echo 'Total: ', $total, "\n";
+	echo 'Ribbon: ', $ribbontotal, "\n";
