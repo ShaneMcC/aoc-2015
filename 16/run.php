c1ca46feb9269b715cc39754ab67355dd3b20e27
Shane Mc Cormack <shanemcc@gmail.com>
2015-12-16 05:39:45 +0000

    Tidy up Day 16

--- 16/run.php
+++ 16/run.php
@@ -5,61 +5,49 @@
 
 	$sues = array();
 	foreach ($input as $details) {
-		preg_match('#^Sue ([0-9]+): (.*)$#SADi', $details, $m);
-		preg_match_all('#([a-z]+): ([0-9]+)#i', $m[2], $details);
-
-		$sues[$m[1]] = array();
-		for ($i = 0; $i < count($details[0]); $i++) {
-			$sues[$m[1]][$details[1][$i]] = $details[2][$i];
+		if (preg_match('#^Sue ([0-9]+): (.*)$#SADi', $details, $m)) {
+			if (preg_match_all('#([a-z]+): ([0-9]+)#i', $m[2], $values)) {
+				$sues[$m[1]] = array();
+				for ($i = 0; $i < count($values[0]); $i++) {
+					$sues[$m[1]][$values[1][$i]] = $values[2][$i];
+				}
+			}
 		}
 	}
 
-	if (count($sues) == 0) {
-		echo 'There are no sues.', "\n";
-		die();
-	}
+	if (count($sues) == 0) { die('There are no sues.'."\n"); }
 
 	$known = array();
-	$known['children'] = 3;
-	$known['cats'] = 7;
-	$known['samoyeds'] = 2;
-	$known['pomeranians'] = 3;
-	$known['akitas'] = 0;
-	$known['vizslas'] = 0;
-	$known['goldfish'] = 5;
-	$known['trees'] = 3;
-	$known['cars'] = 2;
-	$known['perfumes'] = 1;
+	$known['children'] = array('=', 3);
+	$known['cats'] = array('>', 7);
+	$known['samoyeds'] = array('=', 2);
+	$known['pomeranians'] = array('<', 3);
+	$known['akitas'] = array('=', 0);
+	$known['vizslas'] = array('=', 0);
+	$known['goldfish'] = array('<', 5);
+	$known['trees'] = array('>', 3);
+	$known['cars'] = array('=', 2);
+	$known['perfumes'] = array('=', 1);
 
-	$possible1 = array();
-	$possible2 = array();
-	foreach ($sues as $c => $sue) {
-		$maybe1 = true;
-		$maybe2 = true;
-		foreach ($sue as $thing => $count) {
-			if (!isset($known[$thing])) { continue; }
+	function getSue($sues, $known, $alwaysEquals = true) {
+		foreach ($sues as $c => $sue) {
+			foreach ($sue as $thing => $sueCount) {
+				if (!isset($known[$thing])) { continue; }
+				list($expr, $count) = $known[$thing];
+				if ($alwaysEquals) { $expr = '='; }
 
-			if ($known[$thing] != $count) {
-				$maybe1 = false;
-			}
-
-			if ($thing == 'cats' || $thing == 'trees') {
-				if ($count <= $known[$thing]) {
-					$maybe2 = false;
+				if (($expr == '<' && $count <= $sueCount) || ($expr == '>' && $count >= $sueCount) || ($expr == '=' && $count != $sueCount)) {
+					continue 2;
 				}
-			} else if ($thing == 'pomeranians' || $thing == 'goldfish') {
-				if ($count >= $known[$thing]) {
-					$maybe2 = false;
-				}
-			} else if ($known[$thing] != $count) {
-				$maybe2 = false;
 			}
-
+			return array($c, $sue);
 		}
-		if ($maybe1) { $possible1[$c] = $sue; }
-		if ($maybe2) { $possible2[$c] = $sue; }
+
+		return array(0, array());
 	}
 
+	list($number, $detail) = getSue($sues, $known, true);
+	echo 'Part 1 Sue: ', $number, "\n";
 
-	var_dump($possible1);
-	var_dump($possible2);
+	list($number, $detail) = getSue($sues, $known, false);
+	echo 'Part 2 Sue: ', $number, "\n";
