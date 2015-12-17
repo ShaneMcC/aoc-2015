f80566771317fd751485f00383f5c16367316b16
Shane Mc Cormack <shanemcc@gmail.com>
2015-12-17 05:25:46 +0000

    Tidy up Day 17

--- 17/run.php
+++ 17/run.php
@@ -23,30 +23,17 @@
 		return $result;
 	}
 
-$sets = allSets($containers);
 
-$number = count($containers);
-$count = 0;
-foreach ($sets as $s) {
-	if (array_sum($s) == 150) {
-		$count++;
-	}
-}
+	$quantity = isTest() ? 25 : 150;
+	$validSets = array_filter(allSets($containers), function ($a) use ($quantity) { return array_sum($a) == $quantity; });
+	echo 'Part 1: ', "\n";
+	echo "\t", 'Valid Sets: ', count($validSets), "\n";
 
-echo "Part 1: ", $count, "\n";
+	echo "\n";
 
-$number = count($containers);
-$count = 0;
-foreach ($sets as $s) {
-	if (array_sum($s) == 150) {
-		if (count($s) < $number) {
-			$count = 0;
-			$number = count($s);
-		}
-		if (count($s) == $number) {
-			$count++;
-		}
-	}
-}
-
-echo "Part 2: ", $count, "\n";
+	usort($validSets, function($a,$b) { return count($a) - count($b); });
+	$minCount = count($validSets[0]);
+	$minimumSets = array_filter($validSets, function ($a) use ($minCount) { return count($a) == $minCount; });
+	echo 'Part 2: ', "\n";
+	echo "\t", 'Valid Minimum Sets: ', count($minimumSets), "\n";
+	echo "\t", 'Minimum Containers Required: ', $minCount, "\n";
